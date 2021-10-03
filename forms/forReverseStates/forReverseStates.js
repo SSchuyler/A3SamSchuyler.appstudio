bf2eb783dbcd78ef95b7c371b35a7bb7e2ccd66c
Samuel Schuyler <sjs08496@creighton.edu>
2021-10-03 14:55:21 -0500

    Update forReverseStates.js

--- forms/forReverseStates/forReverseStates.js
+++ forms/forReverseStates/forReverseStates.js
@@ -1,6 +1,6 @@
 
 let states = ['Nebraska', 'Iowa', 'Minnesota', 'South Dakota', 'North Dakota', 'Kansas', 'Oklahoma', 'Texas']
-temp = ''
+temp = ""
 
 for (i = states.length - 1; i >= 0; i--) {
     console.log(states[i])
